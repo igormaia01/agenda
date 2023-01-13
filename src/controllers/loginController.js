6baa7e3cb50cdc8b6ede198b74b781ca97328edb
igormaia01 <igormaia2001@gmail.com>
2023-01-12 21:24:40 -0500

    feat: register route

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -1,4 +1,27 @@
+const Login = require('../models/LoginModel');
+
 exports.index = (req, res) => {
-    res.render('login');
+  res.render('login');
 };
-  
+
+exports.register = async function (req, res) {
+  try {
+    const login = new Login(req.body);
+    await login.register();
+
+    if (login.errors.length) {
+      req.flash('errors', login.errors);
+      req.session.save(function () {
+        return res.redirect('index');
+      });
+      return;
+    }
+    req.flash('success', 'Usuario criado com sucesso!');
+    req.session.save(function () {
+      return res.redirect('index');
+    });
+  } catch (error) {
+    console.log(error);
+    return req.render('404');
+  }
+};
