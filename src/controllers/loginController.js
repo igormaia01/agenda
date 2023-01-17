8b63a24985bc96bf23762611ca15198531076d90
igormaia01 <igormaia2001@gmail.com>
2023-01-16 20:29:41 -0500

    login and logout

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -1,7 +1,8 @@
 const Login = require('../models/LoginModel');
 
 exports.index = (req, res) => {
-  res.render('login');
+  if(req.session.user) return res.render('login-logado')
+  return res.render('login');
 };
 
 exports.register = async function (req, res) {
@@ -25,3 +26,35 @@ exports.register = async function (req, res) {
     return req.render('404');
   }
 };
+
+
+exports.login = async function (req, res) {
+  try {
+    const login = new Login(req.body);
+    await login.login();
+
+    if (login.errors.length) {
+      req.flash('errors', login.errors);
+      req.session.save(function () {
+        return res.redirect('index');
+      });
+      return;
+    }
+
+    req.flash('success', 'Logado com sucesso');
+    req.session.user = login.user;
+
+    req.session.save(function () {
+      return res.redirect('index');
+    });
+  } catch (error) {
+    console.log(error);
+    return req.render('404');
+  }
+};
+
+
+exports.logout = function(req, res) {
+  req.session.destroy();
+  res.redirect('/')
+}
