78b0d64288a4793c8573e64da13730197fb58f97
igormaia01 <igormaia2001@gmail.com>
2023-01-17 22:21:28 -0500

    update contact

--- src/controllers/contatoController.js
+++ src/controllers/contatoController.js
@@ -36,4 +36,28 @@ exports.editIndex = async(req, res) => {
   res.render('contato', {
     contato
   });
-} 
+}
+
+exports.edit = async(req, res) => {
+  try {
+    if(!req.params.id) return res.render('404');
+    const contato = new Contato(req.body);
+    await contato.edit(req.params.id);
+  
+    if(contato.errors.length) {
+      req.flash('errors', contato.errors);
+      req.session.save(function () {
+        return res.redirect(`/contato/index/${req.params.id}`);
+      });
+      return;
+    }
+  
+    req.flash('success', 'Contato atualizado com sucesso');
+    req.session.save(function () {
+      return res.redirect(`/contato/index/${req.params.id}`);
+    });
+  } catch (error) {
+    console.log(error);
+    return res.render('404')
+  }
+}
--- src/models/contatoModel.js
+++ src/models/contatoModel.js
@@ -54,4 +54,13 @@ Contato.prototype.cleanUp = function() {
   };
 }
 
+Contato.prototype.edit = async function(id) {
+  if(typeof id !== 'string') return;
+  this.valida();
+
+  if(this.errors.length) return;
+
+  this.contato = await ContatoModel.findByIdAndUpdate(id, this.body, { new: true });
+}
+
 module.exports = Contato;
